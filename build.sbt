1f5f0a0faf94865b3e9f66e075b9d474a1629071
OtsukaNoboru <mopinfish@gmail.com>
2018-01-22 22:47:09 +0900

    [chatwork-notifier] get room info

--- build.sbt
+++ build.sbt
@@ -1,12 +1,21 @@
 import Dependencies._
 
+resolvers += Resolver.sonatypeRepo("snapshots")
+
 lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
       organization := "com.example",
-      scalaVersion := "2.12.3",
+      scalaVersion := "2.11.8",
       version      := "0.1.0-SNAPSHOT"
     )),
     name := "Hello",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies ++= Seq(
+      scalaTest % Test,
+//      "net.cimadai" %% "chatwork-scala" % "1.0.1"
+      "tv.kazu" %% "chatwork4s" % "0.2.5.2",
+      "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
+      "ch.qos.logback" % "logback-classic" % "1.1.3"
+//      "ch.qos.logback" % "logback-classic" % "1.1.9"
+    )
   )
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,8 +1,26 @@
 package example
 
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import tv.kazu.chatwork4s.ChatWorkApiClient
+import tv.kazu.chatwork4s.models._
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+import scala.concurrent.duration.Duration
+import scala.util.{ Try, Success, Failure }
+
 object Hello extends Greeting with App {
-  println(greeting)
-  println("hogehogehoge")
+  val logger = Logger(LoggerFactory.getLogger("logger_name"))
+  val token: String = sys.env("CHATWORK_TOKEN")
+  val client = new ChatWorkApiClient(token)
+  val f: Future[Room] = client.room(361427)
+
+  f.onSuccess { case room: Room => println(room) }
+  f.onFailure { case t: Throwable => println(t.getMessage()) }
+  println(f.isCompleted)
+
+  Await.ready(f, Duration.Inf)
 }
 
 trait Greeting {
